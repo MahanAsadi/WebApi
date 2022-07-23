782a1de96ad978cea47922730bc755b15e57bb22
ITCity <ITCity@DESKTOP-MBNODK0>
2022-07-23 12:35:37 +0430

    Add project files.

---
+++ Middlewares/LogMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Middlewares
+{
+    public class LogMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public LogMiddleware(RequestDelegate next, ILoggerFactory logFactory)
+        {
+            _next = next;
+
+            _logger = logFactory.CreateLogger("MyMiddleware");
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            _logger.LogInformation("log midle executing..");
+
+            await _next(httpContext); // calling next middleware
+
+        }
+    }
+}
